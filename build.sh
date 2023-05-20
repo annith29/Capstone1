a7ae11319bb9470bfb1ebfe5619ffaf411fcebb2
annith29 <118158857+annith29@users.noreply.github.com>
2023-05-20 16:19:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,6 @@ EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
 EOT
 
-
-
 cat Dockerfile
-
-
 echo "Now we will create an image using this docker file with when function using jenkins "
+docker build -t annith29/capstone-public-dev:${BUILD_NUMBER} .
