b4adba0fc2b792f11441e06209fa45129c55016b
annith29 <118158857+annith29@users.noreply.github.com>
2023-05-20 18:00:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,6 @@
-cat <<EOT >Dockerfile
-FROM node:16-alpine as build
-WORKDIR /capapp
-COPY package*.json /capapp/
-RUN npm install
-COPY . .
-RUN npm run build
-FROM nginx:latest
-COPY --from=build /capapp/build/ /usr/share/nginx/html/
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
-EOT
+#!/bin/bash
 
-
-
-cat Dockerfile
-
-
-echo "Now we will create an image using this docker file with when function using jenkins "
-docker build -t annith29/capstone-public-dev:${BUILD_NUMBER} .
-
-
-
+apt-get update && apt install docker.io -y 
+docker --version
+docker build -t reactapp:latest .
+docker images
